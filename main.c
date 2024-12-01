f720d1e85d3b1ef1e7724f2e6b9c6a7dc4120ed5
Ayoubbezai2024 <158614538+AYOUBBB123@users.noreply.github.com>
2024-12-01 10:21:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,15 +10,15 @@ void swap(int *a,int *b){
 }
 
 
-void bubbleSort(int arr[] ,int n){
-    for(int i=0 ; i< n-1 ; i++){
+void bubbleSort(int arr[] , int n){
+    for (int i = 0; i < n-1; ++i) {
         bool swapped = false;
-        for( int j =0 ; j< n-1-i ;j++ ){
+        for (int j = 0; j < n-i-1; ++j) {
             if(arr[j]>arr[j+1]){
                 swap(&arr[j],&arr[j+1]);
                 swapped = true;
-
             }
+
         }
         if(!swapped){
             break;
@@ -26,33 +26,114 @@ void bubbleSort(int arr[] ,int n){
     }
 }
 
-void insertion(int arr[],int n){
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i-1;
-        while (j >=0 && (arr[j] > key) ){
-            arr[j+1]=arr[j];
-            j= j-1;
+int partition(int arr[] , int low , int high){
+
+    int pivotVal = arr[high];
+    int i = low;
+    for (int k = low ; k<high ; k++ ){
+        if(arr[k] <= pivotVal){
+            swap(&arr[i],&arr[k]);
+            i++;
         }
-        arr[j+1]= key;
+    }
+    swap(&arr[high],&arr[i]);
+    return i;
+}
+
+void quickSortRecursion(int arr[] , int low , int high){
+
+    if (low < high){
+        int pivotIndex = partition(arr,low,high);
+        quickSortRecursion(arr, low , pivotIndex-1);
+        quickSortRecursion(arr, pivotIndex+1 , high);
+
     }
 }
+
+
+void quickSort(int arr[] , int length){
+
+    quickSortRecursion(arr, 0 , length-1);
+
+}
+
+
+
+void merge(int arr[] , int left ,int mid ,int right){
+
+    int n1 = mid -left +1;
+    int n2 = right -mid;
+    int leftArr[n1],rightArr[n2];
+
+    for (int i = 0; i < n1; ++i) {
+        leftArr[i] = arr[left+i];
+    }
+    for (int i= 0; i < n1; ++i) {
+        rightArr[i] = arr[mid+1+i];
+    }
+
+    int i = 0 , j= 0;
+    for(int k = left ; k <= right ;k++){
+        if (i<n1 &&(j>=n2 || leftArr[i] <= rightArr[j] )){
+            arr[k] = leftArr[i];
+            i++;
+        }else{
+            arr[k] = rightArr[j];
+            j++;
+        }
+    }
+}
+
+void mergesort(int arr[],int left ,int right){
+    if(left < right){
+        int mid = left +(right - left ) / 2;
+        mergesort(arr , left ,mid);
+        mergesort(arr , mid+1 ,right);
+        merge(arr, left ,mid , right);
+
+    }
+}
+
+
+void insertion(int arr[] , int n ){
+    for(int i = 1 ; i<n ; i++){
+       int key = arr[i];
+       int j =i-1 ;
+
+        while (j>=0 && (arr[j] > key) ){
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+        arr[j+1] = key;
+    }
+
+}
+
+
+void selection(int arr[] , int n){
+    for (int i = 0; i < n-1; ++i) {
+        int min = i ;
+        for (int j = i+1; j < n; ++j) {
+            if(arr[min] > arr[j]){
+                min= j;
+            }
+        }
+
+        if(min != i){
+            swap(&arr[i],&arr[min]);
+
+        }
+    }
+}
+
 
 void display(int arr[],int n){
     for(int i=0 ; i<n ;i++){
         printf("A[%d] = %d \n",i+1,arr[i]);
     }
 }
-// Main function
+
+
 int main() {
-    int arr[5] = { 13, 9, 2, 4, 5 };
-    insertion(arr,5);
-    display(arr,5);
-
-    printf("bubble sort \n");
-    int arr2[6] = { 4, 19, 32, 1, 15,22 };
-    bubbleSort(arr2,6);
-    display(arr2,5);
-
-    return 0;
+  
 }
